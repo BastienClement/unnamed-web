150918767fea84209c9697bf0f41f4cca5410da7
galedric <g@ledric.me>
2012-09-17 00:25:03 +0200

    Fix ranking

---
+++ externals/ranking.php
@@ -0,0 +1,28 @@
+<?php
+
+$URL = "http://www.wowprogress.com/guild/eu/mar%C3%A9cage-de-zangar/The+Unnamed/rating.tier13_10";
+
+$REGEX_WORLD    = "/world:.*?(\d+)/msi";
+$REGEX_EU       = "/eu:.*?(\d+)/msi";
+$REGEX_FR       = "/fr:.*?(\d+)/msi";
+$REGEX_REALM    = "/realm:.*?(\d+)/msi";
+$REGEX_PROGRESS = "/progress:.*?>\s*(\d+\s*\/[^<]+)/msi";
+
+$source = file_get_contents($URL);
+
+if(preg_match($REGEX_WORLD, $source, $preg_world) &&
+   preg_match($REGEX_EU, $source, $preg_eu) &&
+   preg_match($REGEX_FR, $source, $preg_fr) &&
+   preg_match($REGEX_REALM, $source, $preg_realm) &&
+   preg_match($REGEX_PROGRESS, $source, $preg_progress))
+{
+	$ranking = array(
+		"world"    => $preg_world[1],
+		"eu"       => $preg_eu[1],
+		"fr"       => $preg_fr[1],
+		"realm"    => $preg_realm[1],
+		"progress" => $preg_progress[1],
+	);
+
+	file_put_contents(dirname(__FILE__) . "/data/ranking.txt", serialize($ranking));
+}
--- home-blocks/ranking.php
+++ home-blocks/ranking.php
@@ -1,27 +1,27 @@
 <h2>Classement 10 joueurs</h2>
 
-<?php //A MODIFIER AVEC LE FICHIER SERVEUR
-	include("../www/ranking_cache.php");
-	$RANKING_TYPE = preg_match("/H/", $RANKING_PROGRESS) ? "HM" : "NM";
-	$RANKING_PROGRESS = preg_replace("/\(.*\)/", "", $RANKING_PROGRESS);
+<?php
+	$ranking = unserialize(load_external('ranking'));
+	$ranking_type = preg_match("/H/", $ranking['progress']) ? "HM" : "NM";
+	$ranking['progress'] = preg_replace("/\(.*\)/", "", $ranking['progress']);
 ?>
 <div id="rankingWrapper">
 	<div id="rankingServer">
-		<span><? echo $RANKING_REALM;?></span>
+		<span><?php echo $ranking['realm']; ?></span>
 		<div id="rankingServerLabel" style="">Serveur</div>
 	</div>
 	<table style="width: 100%;">
 		<tr>
 			<td style="width: 50%;">
-				<div id="rankingProgress"><span><? echo $RANKING_PROGRESS;?></span> <?php echo $RANKING_TYPE; ?></div>
+				<div id="rankingProgress"><span><?php echo $ranking['progress']; ?></span> <?php echo $ranking_type; ?></div>
 			</td>
 			<td style="width: 50%;">
-				<div id="rankingFr"><span><? echo $RANKING_FR;?></span> FR</div>
+				<div id="rankingFr"><span><?php echo $ranking['fr']; ?></span> FR</div>
 			</td>
 		</tr>
 		<tr>
 			<td style="width: 50%;">
-				<div id="rankingRegion"><span><? echo $RANKING_WORLD;?></span> World</div>
+				<div id="rankingRegion"><span><?php echo $ranking['world']; ?></span> World</div>
 			</td>
 			<td style="width: 50%;">
 				<div id="rankingLink"><a href="http://www.wowprogress.com/guild/eu/mar%C3%A9cage-de-zangar/The+Unnamed/rating.tier13_10">WowProgress</a></div>
