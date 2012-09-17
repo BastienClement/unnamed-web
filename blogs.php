a2c986096ae82c3b3958147d3b131a4c486abea9
David Doyen <doyen.david@gmail.com>
2012-09-17 10:57:10 +0200

    Menu links correction + Basic base pages layout

---
+++ blogs.php
@@ -0,0 +1,15 @@
+<?php
+
+define("HIDE_BLOGS_SLIDER",true);
+
+include('layout/header.php'); ?>
+
+	
+	<div class="section">
+		<div class="section-light">
+			<div class="section-content">
+			</div>
+		</div>
+
+	
+<?php include('layout/footer.php'); ?>
