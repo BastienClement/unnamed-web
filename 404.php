4b31763d2f254140b09ae802b8fb391bf0883496
galedric <g@ledric.me>
2012-09-17 16:26:24 +0200

    404 page

---
+++ 404.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Unnamed</title>
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+			color: #bbb;
+		}
+	
+		body {
+			background:url('/layout/img/background.png');
+		}
+		
+		img {
+			border:none;
+		}
+		
+		#squares {
+			background-image: url('/layout/img/background-top.png');
+			height: 456px;
+			position: absolute;
+			top: 0px;
+			left: 0px;
+			right: 0px;
+			z-index: -1;
+		}
+		
+		#logo {
+			width: 650px;
+			height: 400px;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			margin-left: -325px;
+			margin-top: -200px;
+		}
+		
+	</style>
+</head>
+<body>
+	<div id="squares"></div>
+	<div id="logo">
+		<a href="http://www.unnamed.eu/"><img src="/layout/img/404.png" /></a>
+	</div>
+</body>
+</html>
