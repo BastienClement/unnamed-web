e0c6996c21b3cd9a4298f5eb2edfdd1d48f78d45
David Doyen <doyen.david@gmail.com>
2012-09-22 16:00:52 +0200

    Comments layout + Social sharing icons

--- article.php
+++ article.php
@@ -39,8 +39,23 @@ include('layout/header.php');
 
 <h2>Commentaires</h2>
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue sem a dui iaculis a ultrices lacus facilisis. Nulla iaculis mauris eget magna placerat aliquet. Maecenas feugiat egestas eros sit amet adipiscing. In ac massa eget felis tincidunt pharetra ut sed metus. Curabitur at lectus neque, ut tempus justo. Vivamus mollis nisl et mi egestas porta. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer mattis, felis id faucibus tincidunt, tellus neque sollicitudin enim, id vestibulum dui magna a magna. Vivamus pretium porttitor interdum. Vivamus pharetra, dolor pulvinar vestibulum egestas, mauris lacus ultrices nulla, quis ullamcorper orci tellus quis nisi.
+<div class="comment-block">
+<div class="author-avatar"><a href=""><img src="http://www.unnamed.eu/avatars/243"/></a></div>
+<div class="comment"><div class="comment-top"><span class="comment-actions"><a href=""><i class=" icon-retweet"></i> Citer</a></span><a href="">Aarak</a> <span class="comment-date"><i class=" icon-calendar"></i> 17/09/2010 <i class=" icon-time"></i> 15h09</span></div>
+Curabitur purus dolor, vehicula vestibulum pretium non, placerat eget nisl. Ut quis euismod augue. Donec mollis imperdiet mollis. Curabitur vel rutrum nulla. Vestibulum bland</div>
+</div>
 
+<div class="comment-block">
+<div class="author-avatar"><a href=""><img src="http://www.unnamed.eu/avatars/2"/></a></div>
+<div class="comment"><div class="comment-top"><span class="comment-actions"><a href=""><i class=" icon-retweet"></i> Citer</a></span><a href="">Noumah</a> <span class="comment-date"><i class=" icon-calendar"></i> 17/09/2010 <i class=" icon-time"></i> 15h09</span></div>
+<p>Curabitur purus dolor, vehicula vestibulum pretium non, placerat eget nisl. Ut quis euismod augue. Donec mollis imperdiet mollis. Curabitur vel rutrum nulla. Vestibulum blandit risus at massa semper lacinia. In molestie sollicitudin faucibus. Curabitur semper ante massa, sed cursus augue.Curabitur purus dolor.</p><p>Vehicula vestibulum pretium non, placerat eget nisl. Ut quis euismod augue. Donec mollis imperdiet mollis. Curabitur vel rutrum nulla. Vestibulum blandit risus at massa semper lacinia. In molestie sollicitudin faucibus. Curabitur semper ante massa, sed cursus augue.Curabitur purus dolor, vehicula vestibulum pretium non, placerat eget nisl. Ut quis euismod augue.</p><p> Donec mollis imperdiet mollis. Curabitur vel rutrum nulla. Vestibulum blandit risus at massa semper lacinia. In molestie sollicitudin faucibus. Curabitur semper ante massa, sed cursus augue.Curabitur purus dolor, vehicula vestibulum pretium non, placerat eget nisl. Ut quis euismod augue. Donec mollis imperdiet mollis. Curabitur vel rutrum nulla. Vestibulum blandit risus at massa semper lacinia. In molestie sollicitudin faucibus. Curabitur semper ante massa, sed cursus augue.</p></div>
+</div>
+
+<div class="comment-block">
+<div class="author-avatar"><a href=""><img src="http://www.unnamed.eu/avatars/215"/></a></div>
+<div class="comment"><div class="comment-top"><span class="comment-actions"><a href=""><i class=" icon-retweet"></i> Citer</a></span><a href="">Blash</a> <span class="comment-date"><i class=" icon-calendar"></i> 17/09/2010 <i class=" icon-time"></i> 15h09</span></div>
+<p>Curabitur purus dolor, vehicula vestibulum pretium non, placerat eget nisl. Ut quis euismod augue. Donec mollis imperdiet mollis. Curabitur vel rutrum nulla.</p><p>Vestibulum blandit risus at massa semper lacinia. In molestie sollicitudin faucibus. Curabitur semper ante massa, sed cursus augue.Curabitur purus dolor, vehicula vestibulum pretium non, placerat eget nisl. Ut quis euismod augue. Donec mollis imperdiet mollis. Curabitur vel rutrum nulla. Vestibulum blandit risus at massa semper lacinia. In molestie sollicitudin faucibus. Curabitur semper ante massa, sed cursus augue.</p></div>
+</div>
 
 	<div class="hr" id="leavecomment"></div>
 
@@ -55,15 +70,32 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue sem a dui 
 
 </div>
 
-<div class="col col2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue sem a dui iaculis a ultrices lacus facilisis. Nulla iaculis mauris eget magna placerat aliquet. Maecenas feugiat egestas eros sit amet adipiscing. In ac massa eget felis tincidunt pharetra ut sed metus. Curabitur at lectus neque, ut tempus justo. Vivamus mollis nisl et mi egestas porta. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer mattis, felis id faucibus tincidunt, tellus neque sollicitudin enim, id vestibulum dui magna a magna. Vivamus pretium porttitor interdum. Vivamus pharetra, dolor pulvinar vestibulum egestas, mauris lacus ultrices nulla, quis ullamcorper orci tellus quis nisi.
+<div class="col col2">
 
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc felis neque, interdum eget laoreet sit amet, semper ut eros. Vivamus auctor massa sit amet est aliquet vel mattis elit fringilla. Ut auctor, risus ut pretium eleifend, velit augue rhoncus nisl, laoreet accumsan enim diam non elit. Proin vel orci magna. Donec sit amet dui id lectus ornare lacinia vitae id nibh. Nunc nec est nibh, sit amet cursus quam. Ut ac lorem arcu, tincidunt tempus tortor.
+<h2>Partager cet article</h2>
 
-Vivamus dui nibh, eleifend at pharetra ut, elementum sit amet arcu. Morbi non imperdiet neque. Aliquam sodales ipsum ac nunc cursus ut mollis sem sollicitudin. Pellentesque a felis id justo lobortis bibendum. Aliquam quis fermentum erat. Phasellus ullamcorper, purus ut semper malesuada, enim diam blandit libero, a eleifend libero ligula sit amet dui. In quis nibh sapien. Integer tempus fringilla ullamcorper. Integer vel massa sed est aliquam scelerisque eget non lacus. Nam ullamcorper turpis vel neque dignissim malesuada. Nulla odio erat, tempus in euismod quis, porttitor sed eros.
+<div id="share">
+<a href=""><img src="/layout/img/social-icons/facebook.png" alt="Facebook" title="Facebook"/></a>
+<a href=""><img src="/layout/img/social-icons/twitter.png" alt="Twitter" title="Twitter"/></a>
+<a href=""><img src="/layout/img/social-icons/google.png" alt="Google+" title="Google+"/></a>
+<a href=""><img src="/layout/img/social-icons/mail.png" alt="Email" title="Email"/></a>
+</div>
 
-Vivamus egestas augue ut ligula fermentum quis malesuada mi tincidunt. Fusce suscipit purus faucibus velit lobortis at eleifend nibh rutrum. Cras eu metus elit. Suspendisse quam quam, luctus quis lobortis lobortis, luctus eu massa. Vestibulum ultricies pulvinar eros, nec aliquet velit sagittis at. Donec tempus sem consectetur lectus malesuada auctor. Nullam in libero sit amet risus convallis ultricies. In nibh nibh, vulputate ut congue in, euismod at eros. In orci lacus, luctus ac aliquam non, ornare id odio. Aenean iaculis aliquam leo, ac egestas nisl ullamcorper eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus venenatis quam vel tristique. Integer id nunc ipsum, vitae consectetur lectus. Vestibulum viverra dolor sit amet ligula vulputate convallis.
+<div class="clearfix"></div>
 
-Curabitur purus dolor, vehicula vestibulum pretium non, placerat eget nisl. Ut quis euismod augue. Donec mollis imperdiet mollis. Curabitur vel rutrum nulla. Vestibulum blandit risus at massa semper lacinia. In molestie sollicitudin faucibus. Curabitur semper ante massa, sed cursus augue.</div>
+<div class="hr"></div>
+
+<h2>à propos de l'auteur</h2>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue sem a dui iaculis a ultrices lacus facilisis. Nulla iaculis mauris eget magna placerat aliquet. Maecenas feugiat egestas eros sit amet adipiscing. In ac massa eget felis tincidunt pharetra ut sed metus. Curabitur at lectus neque, ut tempus justo. Vivamus mollis nisl et mi egestas porta. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer mattis, felis id faucibus tincidunt, tellus neque sollicitudin enim, id vestibulum dui magna a magna. Vivamus pretium porttitor interdum. Vivamus pharetra, dolor pulvinar vestibulum egestas, mauris lacus ultrices nulla, quis ullamcorper orci tellus quis nisi.
+
+<div class="hr"></div>
+
+<h2>à lire également</h2>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue sem a dui iaculis a ultrices lacus facilisis. Nulla iaculis mauris eget magna placerat aliquet. Maecenas feugiat egestas eros sit amet adipiscing. In ac massa eget felis tincidunt pharetra ut sed metus. Curabitur at lectus neque, ut tempus justo. Vivamus mollis nisl et mi egestas porta. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer mattis, felis id faucibus tincidunt, tellus neque sollicitudin enim, id vestibulum dui magna a magna. Vivamus pretium porttitor interdum. Vivamus pharetra, dolor pulvinar vestibulum egestas, mauris lacus ultrices nulla, quis ullamcorper orci tellus quis nisi.
+
+</div>
 
 <div class="clearfix"></div>
 
