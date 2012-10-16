772be19cb2f414d94840d32f75813e49878198ac
galedric <g@ledric.me>
2012-10-16 02:06:04 +0200

    Fetch ladder from WoWProgress (progress should now be fully automated)

--- blocks/ranking.php
+++ blocks/ranking.php
@@ -1,10 +1,10 @@
-<h2>Classement 10 joueurs</h2>
-
 <?php
 	$ranking = unserialize(load_external('ranking'));
 	$ranking_type = preg_match("/H/", $ranking['progress']) ? "HM" : "NM";
 	$ranking['progress'] = preg_replace("/\(.*\)/", "", $ranking['progress']);
 ?>
+
+<h2>Classement palier T<?php echo $ranking['ladder']; ?></h2>
 <div id="rankingWrapper">
 	<div id="rankingServer">
 		<span><?php echo $ranking['realm']; ?></span>
@@ -24,7 +24,7 @@
 				<div id="rankingRegion"><span><?php echo $ranking['world']; ?></span> World</div>
 			</td>
 			<td style="width: 50%;">
-				<div id="rankingLink"><a href="http://www.wowprogress.com/guild/eu/mar%C3%A9cage-de-zangar/The+Unnamed">WowProgress</a></div>
+				<div id="rankingLink"><a href="<?php echo $ranking['source']; ?>">WowProgress</a></div>
 			</td>
 		</tr>
 	</table>
--- externals/ranking.php
+++ externals/ranking.php
@@ -7,6 +7,7 @@ $REGEX_EU       = "/eu:.*?(\d+)/msi";
 $REGEX_FR       = "/fr:.*?(\d+)/msi";
 $REGEX_REALM    = "/realm:.*?(\d+)/msi";
 $REGEX_PROGRESS = "/progress:.*?>\s*(\d+\s*\/[^<]+)/msi";
+$REGEX_LADDER   = "/<span class=\"selected\">tier(.*?)<\/span>/msi";
 
 $source = file_get_contents($URL);
 
@@ -14,7 +15,8 @@ if(preg_match($REGEX_WORLD, $source, $preg_world) &&
    preg_match($REGEX_EU, $source, $preg_eu) &&
    preg_match($REGEX_FR, $source, $preg_fr) &&
    preg_match($REGEX_REALM, $source, $preg_realm) &&
-   preg_match($REGEX_PROGRESS, $source, $preg_progress))
+   preg_match($REGEX_PROGRESS, $source, $preg_progress) &&
+   preg_match($REGEX_LADDER, $source, $preg_ladder))
 {
 	$ranking = array(
 		"world"    => $preg_world[1],
@@ -22,6 +24,8 @@ if(preg_match($REGEX_WORLD, $source, $preg_world) &&
 		"fr"       => $preg_fr[1],
 		"realm"    => $preg_realm[1],
 		"progress" => $preg_progress[1],
+		"ladder"   => $preg_ladder[1],
+		"source"   => $URL
 	);
 
 	file_put_contents(dirname(__FILE__) . "/data/ranking.txt", serialize($ranking));
