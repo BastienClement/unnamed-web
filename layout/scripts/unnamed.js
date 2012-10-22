646db1e4bf847e5483b39446ef439d98ee8bd644
galedric <g@ledric.me>
2012-10-22 15:07:56 +0200

    xbbc/ucode: updated, components are done.

--- layout/scripts/unnamed.js
+++ layout/scripts/unnamed.js
@@ -4,6 +4,12 @@
 
 ucode = {
 	toggler: function(self) {
-		$(self).parent().toggleClass('toggled');
+		$(self).parent().toggleClass("toggled");
+	},
+	
+	tabs: function(self, id) {
+		var wrapper = $(self).closest(".tabs-wrapper");
+		$(".tab, .tab-content", wrapper).removeClass("active");
+		$(".tab-"+id+", .tab-content-"+id, wrapper).addClass("active");
 	}
 };
