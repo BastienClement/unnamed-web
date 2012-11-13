5021e46f7a1e2f291803ee11891bd9855fa76084
galedric <g@ledric.me>
2012-11-13 18:04:47 +0100

    Fix progress ranking

--- externals/ranking.php
+++ externals/ranking.php
@@ -6,7 +6,7 @@ $REGEX_WORLD    = "/world:.*?(\d+)/msi";
 $REGEX_EU       = "/eu:.*?(\d+)/msi";
 $REGEX_FR       = "/fr:.*?(\d+)/msi";
 $REGEX_REALM    = "/realm:.*?(\d+)/msi";
-$REGEX_PROGRESS = "/progress:.*?>\s*(\d+\s*\/[^<]+)/msi";
+$REGEX_PROGRESS = "/progress:.*?>(.*?)<\/span>/msi";
 $REGEX_LADDER   = "/<span class=\"selected\">tier(.*?)<\/span>/msi";
 
 $source = file_get_contents($URL);
@@ -18,12 +18,29 @@ if(preg_match($REGEX_WORLD, $source, $preg_world) &&
    preg_match($REGEX_PROGRESS, $source, $preg_progress) &&
    preg_match($REGEX_LADDER, $source, $preg_ladder))
 {
+	preg_match_all('/(\d)\/(\d)( \(H\))?/', strip_tags($preg_progress[1]), $preg_progress, PREG_SET_ORDER);
+	
+	$kill_n = 0;
+	$kill_h = 0;
+	$total = 0;
+	
+	foreach($preg_progress as $raid) {
+		$total += $raid[2];
+		if(isset($raid[3])) {
+			$kill_h += $raid[1];
+		} else {
+			$kill_n += $raid[1];
+		}
+	}
+	
+	$progress = ($kill_h) ? "$kill_h/$total (H)" : "$kill_n/$total";
+	
 	$ranking = array(
 		"world"    => $preg_world[1],
 		"eu"       => $preg_eu[1],
 		"fr"       => $preg_fr[1],
 		"realm"    => $preg_realm[1],
-		"progress" => $preg_progress[1],
+		"progress" => $progress,
 		"ladder"   => $preg_ladder[1],
 		"source"   => $URL
 	);
