092f15130fe38a2f2e59a0cb777442fb0b4a830a
David Doyen <doyen.david@gmail.com>
2012-11-23 00:56:22 +0100

    Recruitement block + css

--- blocks/recruitement.php
+++ blocks/recruitement.php
@@ -1,20 +1,14 @@
 <h2>Recrutement</h2>
 
 <div class="ucode">
-<table class="compact">
-	<tr><td><span class="wow-class c1"><img src="http://wow.zamimg.com/images/wow/icons/small/class_warrior.jpg"> Guerrier</span></td><td width="15"></td><td width="15"><div class="closed"></div></td><td width="15"><div class="closed"></div></td><td width="15"><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c2"><img src="http://wow.zamimg.com/images/wow/icons/small/class_paladin.jpg"> Paladin</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c3"><img src="http://wow.zamimg.com/images/wow/icons/small/class_hunter.jpg"> Chasseur</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c4"><img src="http://wow.zamimg.com/images/wow/icons/small/class_rogue.jpg"> Voleur</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c5"><img src="http://wow.zamimg.com/images/wow/icons/small/class_priest.jpg"> Prêtre</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c6"><img src="http://wow.zamimg.com/images/wow/icons/small/class_deathknight.jpg"> DK</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c7"><img src="http://wow.zamimg.com/images/wow/icons/small/class_shaman.jpg"> Chaman</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c8"><img src="http://wow.zamimg.com/images/wow/icons/small/class_mage.jpg"> Mage</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c9"><img src="http://wow.zamimg.com/images/wow/icons/small/class_warlock.jpg"> Démoniste</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c10"><img src="http://wow.zamimg.com/images/wow/icons/small/class_monk.jpg"> Moine</span></td><td></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
-	<tr><td><span class="wow-class c11"><img src="http://wow.zamimg.com/images/wow/icons/small/class_druid.jpg"> Druide</span></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td><td><div class="closed"></div></td></tr>
+<table>
+	<tr>
+		<td><span class="wow-class c1">Guerrier</span></td><td style="width:18px;"></td>
+		<td style="width:18px;"><img class="class-icon" src="http://wow.zamimg.com/images/wow/icons/small/ability_warrior_savageblow.jpg" title="Armes"/></td>
+		<td style="width:18px;"><img class="class-icon" src="http://wow.zamimg.com/images/wow/icons/small/ability_warrior_innerrage.jpg" title="Fureur"/></td>
+		<td style="width:18px;"><img class="class-icon  recruitement-close" src="http://wow.zamimg.com/images/wow/icons/small/ability_warrior_defensivestance.jpg" title="Protection"/></td>
+	</tr>
 	
 </table>
 
-<div class="button-wrapper"><a href="/progression" class="button">Consulter le recrutement</a></div>
 </div>
