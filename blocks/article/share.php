14a668834d069631c0497647cbb311f55a97ec59
David Doyen <doyen.david@gmail.com>
2012-11-23 23:35:07 +0100

    Sharer workins as intended :lawl:

--- blocks/article/share.php
+++ blocks/article/share.php
@@ -1,11 +1,11 @@
 <h2>Partager <?php echo defined('SHARE_WHAT') ? SHARE_WHAT : 'cet article'; ?></h2>
 
 <div id="share">
-	<a href=""><img src="/layout/img/social-icons/facebook.png" alt="Facebook" title="Facebook"/></a>
-	<a href=""><img src="/layout/img/social-icons/twitter.png" alt="Twitter" title="Twitter"/></a>
-	<a href=""><img src="/layout/img/social-icons/google.png" alt="Google+" title="Google+"/></a>
-	<a href=""><img src="/layout/img/social-icons/mail.png" alt="Email" title="Email"/></a>
-	<a href=""><img src="/layout/img/social-icons/addthis.png" alt="AddThis" title="AddThis"/></a>
+	<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo current_url(); ?>"><img src="/layout/img/social-icons/facebook.png" alt="Facebook" title="Facebook"/></a>
+	<a href="http://twitter.com/home?status=<?php echo current_url(); ?>"><img src="/layout/img/social-icons/twitter.png" alt="Twitter" title="Twitter"/></a>
+	<a href="https://plus.google.com/share?url=<?php echo current_url(); ?>"><img src="/layout/img/social-icons/google.png" alt="Google+" title="Google+"/></a>
+	<a href="mailto:?subject=Un article int&eacute;ressant sur Unnamed.eu&amp;body=<?php echo current_url(); ?>"><img src="/layout/img/social-icons/mail.png" alt="Email" title="Email"/></a>
+	<a rel="shadowbox;width=520" href="http://www.addthis.com/bookmark.php"><img src="/layout/img/social-icons/addthis.png" alt="AddThis" title="AddThis"/></a>
 </div>
 
 <div class="clearfix"></div>
