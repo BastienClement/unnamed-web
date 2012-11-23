3ebaa37eca4c64eb3aee461d1d93bb5920ee6840
galedric <g@ledric.me>
2012-11-23 11:30:46 +0100

    Using new parser features

--- blocks/comments.php
+++ blocks/comments.php
@@ -13,7 +13,20 @@ if(defined('BLOCK_COMMENTS_FORUM'))
 $result = $db->query("SELECT t.id, t.subject, t.forum_id, p.id AS post_id, p.poster_id, p.poster, p.message, p.posted, pu.slug FROM posts AS p INNER JOIN topics AS t ON t.id = p.topic_id INNER JOIN users AS u ON u.id = p.poster_id INNER JOIN users AS pu ON pu.username = t.poster WHERE t.forum_id = 16 OR (t.forum_id = 18 AND pu.slug != '') AND p.id != t.first_post_id $WHERE ORDER BY p.posted DESC LIMIT 5");
 
 $xbbc = xbbc_ucode_parser();
-//$xbbc->SetFlag(\XBBC\PLAIN_TEXT);
+$xbbc->SetFlag(\XBBC\NO_HTMLESC);
+$xbbc->SetFlag(\XBBC\PLAIN_TEXT);
+
+$xbbc->CustomReducer(function($tag, $ctx) {
+	if($tag instanceof \UCode\URoot) {
+		$txt = $tag->ReducePlaintext();
+		return empty($txt) ? '[...]' : $txt;
+	}
+	
+	if($tag->Display() != \XBBC\DISPLAY_INLINE) { return false; }
+	if($tag instanceof \UCode\ImageTag) { return false; }
+	
+	return $tag->ReducePlaintext();
+});
 
 while($cur_topic = $db->fetch_assoc($result)) {
 	$date = '<abbr class="timeago-uc" title="'.date('c', $cur_topic['posted']).'">'.date('d/m/Y H:i', $cur_topic['posted']).'</abbr>';
