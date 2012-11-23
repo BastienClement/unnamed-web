a63360899b2faa3e601cddd19965658cd74b7cb0
galedric <g@ledric.me>
2012-11-23 22:10:55 +0100

    Fix broken last comments

--- blocks/comments.php
+++ blocks/comments.php
@@ -7,10 +7,10 @@ $comments = array();
 $WHERE = '';
 if(defined('BLOCK_COMMENTS_FORUM'))
 	$WHERE .= ' AND t.forum_id = '.BLOCK_COMMENTS_FORUM;
-	if(defined('BLOCK_COMMENTS_AUTHOR'))
+if(defined('BLOCK_COMMENTS_AUTHOR'))
 	$WHERE .= ' AND pu.id = '.BLOCK_COMMENTS_AUTHOR;
 
-$result = $db->query("SELECT t.id, t.subject, t.forum_id, p.id AS post_id, p.poster_id, p.poster, p.message, p.posted, pu.slug FROM posts AS p INNER JOIN topics AS t ON t.id = p.topic_id INNER JOIN users AS u ON u.id = p.poster_id INNER JOIN users AS pu ON pu.username = t.poster WHERE t.forum_id = 16 OR (t.forum_id = 18 AND pu.slug != '') AND p.id != t.first_post_id $WHERE ORDER BY p.posted DESC LIMIT 5");
+$result = $db->query("SELECT t.id, t.subject, t.forum_id, p.id AS post_id, p.poster_id, p.poster, p.message, p.posted, pu.slug FROM posts AS p INNER JOIN topics AS t ON t.id = p.topic_id INNER JOIN users AS u ON u.id = p.poster_id INNER JOIN users AS pu ON pu.username = t.poster WHERE (t.forum_id = 16 OR (t.forum_id = 17 AND pu.slug != '')) AND p.id != t.first_post_id $WHERE ORDER BY p.posted DESC LIMIT 5");
 
 $xbbc = xbbc_ucode_parser();
 $xbbc->SetFlag(\XBBC\NO_HTMLESC);
@@ -40,7 +40,7 @@ while($cur_topic = $db->fetch_assoc($result)) {
 	
 	$comments[] = array(
 		$url,
-		truncate($xbbc->Parse($cur_topic['message']), 65, '...', true),
+		truncate($xbbc->Parse($cur_topic['message']), 65, ' ...', true),
 		"$date $author",
 		(((time() - $cur_topic['last_post']) < 86400) ? 'time' : 'calendar')
 	);
